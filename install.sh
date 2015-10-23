91b3caa7cbb96a2660a0d3b164e92e72b241cc91
minggrim <minggrim@gmail.com>
2015-10-23 22:34:35 +0800

    complete install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #This is a script to build a convience enviroment
 cd $(pwd)
 RSYNC=$(which rsync)
@@ -15,9 +15,20 @@ fi
 echo "using $COPY_TOOL to copy files"
 if [ "$RSYNC" = "Y" ]
 then
-	
-else
+	$COPY_TOOL -av --delete vim/.vim ~
+	$COPY_TOOL -av --delete vim/.vimrc ~
+	$COPY_TOOL -av --delete .tmux.conf ~
+	$COPY_TOOL -av --delete bash/.bashrc ~
+	$COPY_TOOL -av --delete bash/.bash_profile ~
+	$COPY_TOOL -av --delete ssh/config ~/.ssh
 
+else
+	$COPY_TOOL vim/.vim ~
+	$COPY_TOOL vim/.vimrc ~
+	$COPY_TOOL .tmux.conf ~
+	$COPY_TOOL bash/.bashrc ~
+	$COPY_TOOL bash/.bash_profile ~
+	$COPY_TOOL ssh/config ~/.ssh
 fi
 
 
